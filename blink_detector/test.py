6b16dcc0e26eecab3360abe01163beb016c16a01
prohor33 <prohorgladkikh@gmail.com>
2016-11-20 02:12:31 +0300

    trying threshold, canny and sobel

---
+++ blink_detector/test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import cv2
+import numpy as np
+
+# built-in module
+import sys
+
+
+if __name__ == '__main__':
+    print(__doc__)
+
+    try:
+        fn = sys.argv[1]
+    except:
+        fn = 0
+
+    def nothing(*arg):
+        pass
+
+    cv2.namedWindow('edge')
+    cv2.namedWindow('res')
+    cv2.namedWindow('res1')
+    cv2.namedWindow('res2')
+    cv2.moveWindow('res', 250, 100)
+    cv2.moveWindow('res1', 0, 300)
+    cv2.moveWindow('res2', 250, 300)
+    cv2.createTrackbar('thrs1', 'edge', 1, 20, nothing)
+    # cv2.createTrackbar('thrs1', 'edge', 0, 250, nothing)
+    # cv2.createTrackbar('thrs1', 'edge', 2000, 5000, nothing)
+    cv2.createTrackbar('thrs2', 'edge', 4000, 5000, nothing)
+
+    img = cv2.imread('../data/eyes_found_tmp/0.jpg')
+    if img is None:
+        print('warning: no image')
+    img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
+
+    while True:
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        thrs1 = cv2.getTrackbarPos('thrs1', 'edge')
+        thrs2 = cv2.getTrackbarPos('thrs2', 'edge')
+        # edge = cv2.Canny(img, thrs1, thrs2, apertureSize=5)
+        # retv, edge = cv2.threshold(img, thresh=thrs1, maxval=2,  type=cv2.THRESH_TOZERO_INV)
+        res_x = cv2.Sobel(img, ddepth=-1, dx=1, dy=0)
+        res_y = cv2.Sobel(img, ddepth=-1, dx=0, dy=1)
+        res = cv2.addWeighted(res_x, 1.0, res_y, 1.0, 0)
+
+        vis = img.copy()
+        vis = np.uint8(vis/2.)
+        # vis[edge != 0] = (0, 255, 0)
+        cv2.imshow('edge', vis)
+        cv2.imshow('res', res)
+        cv2.imshow('res1', res_x)
+        cv2.imshow('res2', res_y)
+        ch = cv2.waitKey(5) & 0xFF
+        if ch == 27:
+            break
+    cv2.destroyAllWindows()
